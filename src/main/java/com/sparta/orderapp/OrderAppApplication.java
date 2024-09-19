2ae63848349d44843a54f3940c9de3559b6e3812
ilmechaJu <pawor006@naver.com>
2024-09-19 16:50:31 +0900

    feat : init server connect Test

--- src/main/java/com/sparta/orderapp/OrderAppApplication.java
+++ src/main/java/com/sparta/orderapp/OrderAppApplication.java
@@ -2,8 +2,10 @@ package com.sparta.orderapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
 public class OrderAppApplication {
 
     public static void main(String[] args) {
